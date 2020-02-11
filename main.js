8e9593ec5dc9c200d6408671be2b66a0f472f0f5
gcmpccnn <giacomopiccinini99@gmail.com>
2020-02-11 13:15:52 +0100

    nav working, have to fix grid

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-fetch("https://kea-alt-del.dk/t5/api/productlist")
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        showData(data)
-    })
+fetch("https://kea-alt-del.dk/t5/api/categories")
+    .then(res => res.json())
+    .then(categories)
 
+
+function categories(data) {
+    data.forEach(function (cat) {
+        const link = document.createElement("a");
+        link.setAttribute("href", `#${cat}`);
+        link.textContent = cat;
+        document.querySelector("nav").appendChild(link)
+
+        const section = document.createElement("section");
+        section.id = cat;
+        const h2 = document.createElement("h2");
+        h2.textContent = cat;
+        section.appendChild(h2);
+
+        document.querySelector("main").appendChild(section);
+    })
+    getProducts();
+}
+
+
+
+function getProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            showData(data)
+        })
+}
 
 
 function showData(jsonData) {
@@ -15,14 +41,23 @@ function showData(jsonData) {
 }
 
 function showDish(dish) {
-const template = document.querySelector("template").content;
+    const template = document.querySelector("template").content;
     const clone = template.cloneNode(true);
     clone.querySelector("h1.name").textContent = dish.name;
     clone.querySelector("p.description").textContent = dish.shortdescription;
     clone.querySelector("img.picture").src = "https://kea-alt-del.dk/t5/site/imgs/" + "small/" + dish.image + "-sm.jpg";
-    clone.querySelector("p.discount").textContent = dish.discount;
-    clone.querySelector("p.price").textContent = dish.price + " dkk";
+    if (dish.discount) {
+        clone.querySelector("h3.price span").textContent = dish.price;
+        clone.querySelector("h2.discount span").textContent = Math.round(dish.price - dish.price * dish.discount / 100);
+    } else {
+        clone.querySelector("h3.price").remove();
+        clone.querySelector("h2.discount span").textContent = dish.price
+        clone.querySelector("img.sale").remove();
+    }
 
-    const parent = document.querySelector("main");
+    //    clone.querySelector("p.discount").textContent = dish.discount;
+    //    clone.querySelector("p.price").textContent = dish.price + " dkk";
+
+    const parent = document.querySelector(`#${dish.category}`);
     parent.appendChild(clone)
 }
